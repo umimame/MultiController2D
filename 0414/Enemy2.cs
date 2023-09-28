a9a75ad5b5b9e4f86643e3f4e316c8feadc97342
ci0414kk <22ci0414@jec.ac.jp>
2023-09-28 12:29:24 +0900

    移動時の効果音の追加　沸き範囲の調整

--- 0414/Enemy2.cs
+++ 0414/Enemy2.cs
@@ -2,24 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
+using UnityEngine.UI;
 
 public class Enemy2 : MonoBehaviour
 {
     private float speed;
     public int hp;
-    private Rigidbody2D rigidBody;
     [SerializeField] private Transform self;
     [SerializeField] private Transform target;
     // 爆発効果音
+
+
     public AudioClip explosionSE;
+
+    //移動中の音
+    public AudioSource moveSE;
+    //public AudioClip moveSE2;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 5f;
+        speed = 15f;
         hp = 10;
         AssignPlayerAsTarget();
-        rigidBody = GetComponent<Rigidbody2D>();
+        MoveSE();
     }
 
     // Update is called once per frame
@@ -31,12 +37,13 @@ public class Enemy2 : MonoBehaviour
             this.hp -= 5;
             if (this.hp <= 0)
             {
-                Destroy(this.gameObject);
+                DestroySelf();
             }
         }
     }
     void Move()
     {
+
         Vector3 dir = (target.position - self.position);
 
         self.rotation = Quaternion.FromToRotation(Vector3.up, dir);
@@ -63,12 +70,30 @@ public class Enemy2 : MonoBehaviour
     [ContextMenu("Assign Player as Target")]
     private void AssignPlayerAsTarget()
     {
+        //Playerタグを持つオブジェクトをターゲットに設定する
         target = GameObject.Find("Player").transform;
     }
     void DestroySelf()
     {
+        ExplosionSE();
+        //自身を破壊
+        Destroy(this.gameObject);
+    }
+    void MoveSE()
+    {
+        // AudioSourceコンポーネントを取得
+        moveSE = GetComponent<AudioSource>();
+
+        // オーディオが再生可能な場合
+        if (moveSE != null)
+        {
+            // オーディオを再生
+            moveSE.Play();
+        }
+    }
+    void ExplosionSE()
+    {
         // オーディオを再生
         AudioSource.PlayClipAtPoint(explosionSE, this.transform.position);
-        Destroy(this.gameObject);
     }
 }
--- 0414/EnemyGen.cs
+++ 0414/EnemyGen.cs
@@ -7,25 +7,53 @@ public class EnemyGen : MonoBehaviour
     public GameObject EnemyPrefab;
     public int GenSpan;
     private float time;
+    public int GenNum;
+
+    [SerializeField] private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
         GenSpan = 3;
         time = 0;
+        GenNum = 10;
+        AssignPlayerAsTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Random.Range(0f, 10.0f);
-        float y = Random.Range(-5.0f, 5.0f);
         time += Time.deltaTime;
-
         if (time > GenSpan)
         {
-            GameObject enemy = Instantiate(EnemyPrefab);
-            enemy.transform.position = new Vector3(x, y, 0);
-            time = 0f;
+            for (int i = 0; i < GenNum; i++)
+            {
+                float xPlus = Random.Range(15.0f, 30.0f);
+                float xMinus = Random.Range(-15.0f, -30.0f);
+                float y = Random.Range(-30.0f, 30.0f);
+                int RL = Random.Range(0, 2);
+                if (RL == 0)//プレイヤーを中心に左側に生成
+                {
+                    GameObject enemy = Instantiate(EnemyPrefab);
+                    enemy.transform.position = new Vector3(target.transform.position.x + xMinus,
+                       target.transform.position.y + y, 0);
+                    time = 0f;
+                }
+                else //プレイヤーを中心に右側に生成
+                {
+                    GameObject enemy = Instantiate(EnemyPrefab);
+                    enemy.transform.position = new Vector3(target.transform.position.x + xPlus,
+                       target.transform.position.y + y, 0);
+                    time = 0f;
+                }
+
+            }
         }
     }
+    [ContextMenu("Assign Player as Target")]
+    private void AssignPlayerAsTarget()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
 }
