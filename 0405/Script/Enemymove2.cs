3430e67068482be3dfa483ac9f21dc48b32df487
ci <ci@jec.ac.jp>
2023-10-19 11:12:48 +0900

    SE追加

--- 0405/Script/Enemymove2.cs
+++ 0405/Script/Enemymove2.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemymove2 : MonoBehaviour
@@ -7,50 +8,44 @@ public class Enemymove2 : MonoBehaviour
     public float fireRate = 2.0f;  // 弾を撃つ頻度（秒）
     public GameObject bulletPrefab;  // 弾のプレハブ
     public float bulletSpeed = 10.0f;
-    public LayerMask Wall;
+    public AudioClip shootSound; // 弾を撃つSE
     private Transform player1;  // プレイヤーのTransformコンポーネント
     private Transform player2;
     private Rigidbody2D rb;
-    private bool isChasing = false;
     private float nextFireTime; // 次に弾を撃つ時間
+    private AudioSource audioSource; // AudioSourceコンポーネント
+    private Collider2D movementArea;
 
     void Start()
     {
         player1 = GameObject.FindGameObjectWithTag("Player01").transform;  // プレイヤーのTransformを取得
         player2 = GameObject.FindGameObjectWithTag("Player02").transform;
-        rb = GetComponent<Rigidbody2D>();
-        nextFireTime = Time.time + fireRate;// 最初に弾を撃つ時間を設定
+        rb = GetComponent <Rigidbody2D>();
+        nextFireTime = Time.time + fireRate; // 最初に弾を撃つ時間を設定
+        audioSource = GetComponent<AudioSource>(); // AudioSourceコンポーネントを取得
+        movementArea = GetComponent<Collider2D>();
     }
 
     void Update()
-    {       
+    {
         // プレイヤーと敵の距離を計算
         float distanceToPlayer1 = Vector2.Distance(transform.position, player1.position);
-
         float distanceToPlayer2 = Vector2.Distance(transform.position, player2.position);
 
         // プレイヤーが検知範囲内にいるかチェック
-        if (distanceToPlayer1 < detectionRange || distanceToPlayer2 < detectionRange)
+        if (IsWithinDetectionRange(distanceToPlayer1) || IsWithinDetectionRange(distanceToPlayer2))
         {
             Transform targetPlayer = (distanceToPlayer1 < distanceToPlayer2) ? player1 : player2;
             // プレイヤーの方向を向く
             Vector2 direction = (targetPlayer.position - transform.position).normalized;
             rb.velocity = direction * speed;
-            
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, detectionRange ,Wall);
-            if (hit.collider == null)
+
+            // 弾を撃つ処理
+            if (Time.time > nextFireTime)
             {
-                rb.velocity = direction * speed;
-                // 弾を撃つ処理
-                if (Time.time > nextFireTime)
-                {
-                    Shoot(targetPlayer);
-                    nextFireTime = Time.time + fireRate;  // 次に弾を撃つ時間を更新
-                }
-            }
-            else
-            {
-                rb.velocity = Vector2.zero;  // 壁がある場合は停止
+                Shoot(targetPlayer);
+                audioSource.PlayOneShot(shootSound);
+                nextFireTime = Time.time + fireRate; // 次に弾を撃つ時間を更新
             }
         }
         else
@@ -61,7 +56,12 @@ public class Enemymove2 : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
 
+    bool IsWithinDetectionRange(float distanceToPlayer)
+    {
+        // 検知範囲内にいるかどうかを確認
+        return distanceToPlayer <= detectionRange;
     }
 
     void Shoot(Transform targetPlayer)
